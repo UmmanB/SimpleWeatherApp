3eac1e2bbf6bf46aafce15898d7ebdbcc601e33b
Umman Babayev <ummanbabayev1@gmail.com>
2024-08-30 22:49:11 +0400

    Initial Commit

---
+++ SimpleWeatherApp/SimpleWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherAppApp.swift
+//  SimpleWeatherApp
+//
+//  Created by Umman on 30.08.24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
